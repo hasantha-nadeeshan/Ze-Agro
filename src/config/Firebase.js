1f81e4899c4a29677d7f7b0a987f98a6f40e14dc
Hasantha Nadeeshan <hasantha.nadeeshan@gmail.com>
2021-09-20 18:11:11 +0530

    Changed- Firebase New with zeroxa

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 import "firebase/firestore";
 var firebaseConfig = {
-  apiKey: "AIzaSyAK_fynfOl7QOZJbM_Vf2A4ubs-JlGwg1k",
-  authDomain: "path-finder-9370b.firebaseapp.com",
-  databaseURL: "https://path-finder-9370b-default-rtdb.firebaseio.com",
-  projectId: "path-finder-9370b",
-  storageBucket: "path-finder-9370b.appspot.com",
-  messagingSenderId: "698832012074",
-  appId: "1:698832012074:web:356c5041c53ac95a28d453",
-  measurementId: "G-LG3L1N0430"
+  apiKey: "AIzaSyBHzx_mjYmiji9gBeBqKKPQwDnhu2pX7Wo",
+  authDomain: "ze-impak-prod.firebaseapp.com",
+  databaseURL: "https://ze-impak-prod-default-rtdb.firebaseio.com",
+  projectId: "ze-impak-prod",
+  storageBucket: "ze-impak-prod.appspot.com",
+  messagingSenderId: "744781216096",
+  appId: "1:744781216096:web:7e4190e315eada68a6c605",
+  measurementId: "G-GRZ2RX67YJ"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
